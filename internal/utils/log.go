d99b67d4d30b87859a9d6920d4e45c46479f6fe6
Dylan Maassen van den Brink <dylan@mvdb.eu>
2024-02-20 20:39:15 +0100

    Added foundation for logging to a file

--- internal/utils/log.go
+++ internal/utils/log.go
@@ -1,21 +1,42 @@
 package utils
 
 import (
+	"fmt"
+	"io"
 	"log/slog"
 	"os"
+	"time"
 )
 
 var (
-	Log      *slog.Logger
-	LogLevel *slog.LevelVar
+	Log        *slog.Logger
+	LogLevel   *slog.LevelVar
+	logDir     = os.Getenv("LOG_PATH")
+	logOutputs io.Writer
 )
 
 func init() {
+	//If the LOG_PATH environment variable is empty (non-existent) we just write to Stdout
+	if logDir != "" {
+		err := os.Mkdir(logDir, 0744)
+		if err != nil && !os.IsExist(err) {
+			fmt.Println("Failed to create log directory: ", err.Error())
+		}
+		LogFileName := fmt.Sprintf("%s/%s-%s", logDir, "tibby", time.Now().Format("20060102"))
+		LogFile, err := os.OpenFile(LogFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println("Failed to open log file: ", err.Error())
+		}
+		logOutputs = io.MultiWriter(LogFile, os.Stdout)
+	} else {
+		logOutputs = os.Stdout
+	}
+
 	LogLevel = &slog.LevelVar{}
 	opts := slog.HandlerOptions{
 		Level: LogLevel,
 	}
-	handler := slog.NewTextHandler(os.Stdout, &opts)
+	handler := slog.NewTextHandler(logOutputs, &opts)
 
 	Log = slog.New(handler)
 }
